5e13e7e904e4fe2ef87058e1f8f41996a5bfa8c5
Nathan <nathan.public@gmail.com>
2020-10-08 18:59:59 -0400

    all thes tuff, game's done

--- game.js
+++ game.js
@@ -1,4 +1,79 @@
-game = {tasks:[]}
+game = {resources:{},activetask:null}
+
+//GAMESPEED_RATIO = 1 / (5 * 60 * 1000)  // 1 year every 5 minutes in ms
+GAMESPEED_RATIO = 1 / (1 * 1000)  // 1 year every 1 seconds in ms
+
+var generateResource = function(task, resourceName, rawAmount) {
+    if (!game.resources[resourceName]) {
+        game.resources[resourceName] = 0;
+    }
+    game.resources[resourceName] += rawAmount; // this should get complicated with groundhog meta?
+}
+
+var processPassiveTask = function(task, yearsElapsed) {
+    for (resource in task.passiveGeneration) {
+        generateResource(task, resource, task.passiveGeneration[resource]*yearsElapsed);
+    }
+}
+
+var checkVisibleTasks = function() {
+    game.tasks = [];
+    game.alltasks.forEach(task => {
+        if (task.unlock === null || task.level > 0 || task.unlock !== null && task.unlock.task.level >= task.unlock.level) {
+            game.tasks.push(task);
+        }
+    });
+}
+
+var completeTask = function(task) {
+    if (task.completionGeneration != null) {
+        for (resource in task.completionGeneration) {
+            generateResource(task, resource, task.completionGeneration[resource]);
+        }
+    }
+
+    task.level++;
+    // the important part of a completed task is checking if new tasks are unlocked
+    // (one day this might happen outside of task completion/game init 'cause who tf knows what would cause unlocks? maybe?)
+    checkVisibleTasks();
+}
+
+var doTask = function(task) {
+    if (task === null) return;
+    if (game.activetask !== null) return;
+
+    // if we can't do this task for some other reason, return? Not visible (cheaters!)? Not enough money? (need a UI indicator for that when i wrote this...)
+
+    game.activetask = task;
+    task.yearsWorked = 0;
+}
+
+var update = function(elapsed) {
+    if (game.activetask) { // if no active task, we don't update the game! NOT IDLE!
+        var maxYearsElapsed = elapsed * GAMESPEED_RATIO;
+        var remainingYears = game.activetask.completionYears - game.activetask.yearsWorked;
+        var yearsElapsed = Math.min(maxYearsElapsed, remainingYears);
+
+        // continue the active task...
+        game.activetask.yearsWorked += yearsElapsed;
+
+        if (game.activetask.completionYears == game.activetask.yearsWorked) {
+            completeTask(game.activetask);
+            game.activetask = null;
+        }
+
+        // not in V1, but this is probably where automated tasks would go?
+        // game.autotasks.forEach(task => { ...
+        game.tasks.forEach(task => {
+            // if this task is active (level > 0) and generates passive income, we do that here.
+            if (task.level > 0) {
+                if (task.passiveGeneration != null) {
+                    processPassiveTask(task, yearsElapsed);
+                }
+            }
+        });
+    }
+}
 
 var draw = function() {
     var canvas = document.getElementById("canvas");
@@ -22,71 +97,43 @@ var draw = function() {
 
         ctx.drawImage(image, task.x-25, task.y-25, 50, 50);
         // gotta put a progress bar into the image now?
-        if (task.progress < 1) {
-            ctx.globalAlpha=0.1
+        if (task = game.activetask) { // not sure how to indicate it needs a cooldown for non-activetasks (not in V1 anyway)
+            var pctComplete = task.yearsWorked / task.completionYears;
+
+            ctx.globalAlpha=0.3
             ctx.beginPath();
             ctx.moveTo(task.x, task.y);
             ctx.lineTo(task.x, task.y + 25);
-            ctx.arc(task.x, task.y, 25, -0.5*Math.PI, 2 * Math.PI * (task.progress) - 0.5*Math.PI, true);
+            ctx.arc(task.x, task.y, 25, -0.5*Math.PI, 2 * Math.PI * pctComplete - 0.5*Math.PI, true);
             ctx.lineTo(task.x, task.y);
             ctx.fill();
             ctx.globalAlpha=1
-            task.progress = Math.min(1,task.progress+0.003);
         }
     })
 
     ctx.restore();
-    setTimeout(draw, 30);
 }
 
-// original example code
+function loop(timestamp) {
+  var progress = timestamp - lastRender
 
-//var c = document.getElementById("c");
-//
-//c.width = window.innerWidth;
-//c.height = window.innerHeight;
-//
-//var ctx = c.getContext("2d");
-//ctx.moveTo(0, 0);
-//ctx.lineTo(2000, 1000);
-//ctx.stroke();
-//
-//ctx.beginPath();
-//ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-//ctx.stroke();
-//
-//ctx.font = "30px Arial";
-//ctx.fillText("Ugh, World", 100, 50);
+  if (lastRender > 0) {
+      update(progress)
+      draw()
+  }
 
-// if chrome is making weird borders, the internet suggested this (either every time or once as a caching prep?)
-var prepTaskCanvas = function(img) {
-    var tmpCanvas = document.createElement('canvas'),
-        tmpCtx = tmpCanvas.getContext('2d'),
-        image = new Image();
+  lastRender = timestamp
+  window.requestAnimationFrame(loop)
+}
+var lastRender = 0
 
-    image.src = img;
-    console.log(image);
-
-    tmpCanvas.width = image.width*2;
-    tmpCanvas.height = image.height*2;
-
-    // draw the cached images to temporary canvas and return the context
-    tmpCtx.save();
-    tmpCtx.beginPath();
-    tmpCtx.arc(2*24, 2*24, 2*24, 0, Math.PI*2, true);
-    tmpCtx.closePath();
-    tmpCtx.clip();
-
-    tmpCtx.drawImage(image, 0, 0, 4*24+2, 4*24+2);
-
-    tmpCtx.beginPath();
-    tmpCtx.arc(0, 0, 2, 0, Math.PI*2, true);
-    tmpCtx.clip();
-    tmpCtx.closePath();
-    tmpCtx.restore();
-
-    return tmpCanvas;
-};
+function getMousePos(canvas, event) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top
+    };
+}
 
 function startGame() {
     var c = document.getElementById("canvas");
@@ -94,23 +141,57 @@ function startGame() {
 
     var task1 = {};
     task1.imageUrl = 'images/farm1.jpg';
-    task1.x = 50;
-    task1.y = 200;
-    task1.progress = 0.75;
+    task1.x = 250;
+    task1.y = 250;
+    task1.level = 0;
+    task1.repeatable = true;
+    task1.completionGeneration = {wheat:1};
+    task1.completionYears = 1;
+    task1.unlock = null;
 
     var task2 = {};
     task2.imageUrl = 'images/rock.png';
-    task2.x = 100;
-    task2.y = 200;
-    task2.progress = 1;
+    task2.x = 250;
+    task2.y = 300;
+    task2.level = 0;
+    task2.repeatable = false;
+    task2.passiveGeneration = {stone:1};
+    task2.completionGeneration = {stone:50};
+    task2.completionYears = 1.5;
+    task2.unlock = {task: task1, level:4, permanent:true}
 
     var task3 = {};
     task3.imageUrl = 'images/farm2.jpg';
-    task3.x = 200;
-    task3.y = 50;
-    task3.progress = 0.25;
+    task3.x = 250;
+    task3.y = 350;
+    task3.level = 0;
+    task3.repeatable = true;
+    task3.passiveGeneration = {pmoney:2.5, omoney: 0.3};
+    task3.completionGeneration = {wheat:2};
+    task3.completionYears = 0.75;
+    task3.unlock = {task: task2, level:1, permanent:false}
 
-    game.tasks = [task1, task2, task3];
-    draw();
+    game.alltasks = [task1, task2, task3];
 
+    checkVisibleTasks();
+
+    canvas.addEventListener('click', function(evt) {
+        var mousePos = getMousePos(canvas, evt);
+        var clickedTask = null;
+        var closestClick = 25.1;
+        game.tasks.forEach(task => {
+            xd = task.x - mousePos.x;
+            yd = task.y - mousePos.y;
+            d = Math.sqrt(xd*xd+yd*yd)
+            if (d < closestClick) {
+                closestClick = d;
+                clickedTask = task;
+            }
+        });
+
+        doTask(clickedTask);
+
+    }, false);
+
+    window.requestAnimationFrame(loop)
 }
