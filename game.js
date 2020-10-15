16561efbb0e3b14d644499a1a4fd97e5704f73c8
Nathan <nathan.public@gmail.com>
2020-10-15 14:21:45 -0400

    added a version display so i can update it when i push and know it's live

--- game.js
+++ game.js
@@ -27,7 +27,7 @@ var oldMessagesBlock = document.getElementById("messages")
 var latestMessageBlock = document.getElementById("latestMessage")
 var messagesScrollBlock = document.getElementById("messagesScroll")
 
-document.getElementById("jsv").innerHTML = "v0.01";
+document.getElementById("jsv").innerHTML = "v0.02";
 var needRedraw = true; // this is the "UI is dirty" flag, 'cause nobody likes games that run at 100% cpu...
 
 //GAMESPEED_RATIO = 1 / (5 * 60 * 1000)  // 1 year every 5 minutes in ms
